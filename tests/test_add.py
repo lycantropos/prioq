73cc4e243d70303ef19a8b3c4dbd0262ded080a9
Azat Ibrakov <azatibrakov@gmail.com>
2019-12-18 19:10:22 +0300

    Complete test for 'PriorityQueue.add' method

--- tests/test_add.py
+++ tests/test_add.py
@@ -8,6 +8,16 @@ from . import strategies
 
 
 @given(strategies.priority_queues_with_values)
+def test_basic(priority_queue_with_value: Tuple[PriorityQueue, Domain]
+               ) -> None:
+    priority_queue, value = priority_queue_with_value
+
+    result = priority_queue.add(value)
+
+    assert result is None
+
+
+@given(strategies.priority_queues_with_values)
 def test_properties(priority_queue_with_value: Tuple[PriorityQueue, Domain]
                     ) -> None:
     priority_queue, value = priority_queue_with_value
