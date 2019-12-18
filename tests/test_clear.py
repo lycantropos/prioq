5916938eb7f0856e457fb3d11686977fcca5120e
Azat Ibrakov <azatibrakov@gmail.com>
2019-12-18 19:11:11 +0300

    Complete test for 'PriorityQueue.clear' method

--- tests/test_clear.py
+++ tests/test_clear.py
@@ -5,6 +5,13 @@ from . import strategies
 
 
 @given(strategies.priority_queues)
+def test_basic(priority_queue: PriorityQueue) -> None:
+    result = priority_queue.clear()
+
+    assert result is None
+
+
+@given(strategies.priority_queues)
 def test_properties(priority_queue: PriorityQueue) -> None:
     priority_queue.clear()
 
