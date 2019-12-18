4d016bdc604a0dc015ed6570a12f095baee1e133
Azat Ibrakov <azatibrakov@gmail.com>
2019-12-18 19:17:48 +0300

    Add test for 'PriorityQueue.discard' method

---
+++ tests/test_discard.py
@@ -0,0 +1,29 @@
+from copy import deepcopy
+from typing import Tuple
+
+from hypothesis import given
+
+from prioq.base import PriorityQueue
+from prioq.hints import Domain
+from tests import strategies
+
+
+@given(strategies.priority_queues_with_values)
+def test_basic(priority_queue_with_value: Tuple[PriorityQueue, Domain]
+               ) -> None:
+    priority_queue, value = priority_queue_with_value
+
+    result = priority_queue.discard(value)
+
+    assert result is None
+
+
+@given(strategies.priority_queues_with_values)
+def test_properties(priority_queue_with_value: Tuple[PriorityQueue, Domain]
+                    ) -> None:
+    priority_queue, value = priority_queue_with_value
+    original = deepcopy(priority_queue)
+
+    priority_queue.discard(value)
+
+    assert len(priority_queue) == len(original) - (value in original)
