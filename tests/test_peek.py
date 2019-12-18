fd23e5e9b9b87f0144e12e6807e902f19eed0b15
Azat Ibrakov <azatibrakov@gmail.com>
2019-12-18 19:07:37 +0300

    Add test for 'PriorityQueue.peek' method

---
+++ tests/test_peek.py
@@ -0,0 +1,20 @@
+import pytest
+from hypothesis import given
+
+from prioq.base import PriorityQueue
+from . import strategies
+
+
+@given(strategies.empty_priority_queues)
+def test_base_case(priority_queue: PriorityQueue) -> None:
+    with pytest.raises(KeyError):
+        priority_queue.peek()
+
+
+@given(strategies.non_empty_priority_queues)
+def test_step(priority_queue: PriorityQueue) -> None:
+    result = priority_queue.peek()
+
+    assert result in priority_queue
+    assert all(not item < priority_queue._value_to_item(result)
+               for item in priority_queue._items)
