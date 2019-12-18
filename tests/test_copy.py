f880eb6e226670ca54cb95ba59045e24019d758d
Azat Ibrakov <azatibrakov@gmail.com>
2019-12-18 17:53:30 +0300

    Add test for 'PriorityQueue' class copying

---
+++ tests/test_copy.py
@@ -0,0 +1,22 @@
+import copy
+
+from hypothesis import given
+
+from prioq.base import PriorityQueue
+from . import strategies
+
+
+@given(strategies.priority_queues)
+def test_shallow(priority_queue: PriorityQueue) -> None:
+    result = copy.copy(priority_queue)
+
+    assert result is not priority_queue
+    assert result == priority_queue
+
+
+@given(strategies.priority_queues)
+def test_deep(priority_queue: PriorityQueue) -> None:
+    result = copy.deepcopy(priority_queue)
+
+    assert result is not priority_queue
+    assert result == priority_queue
