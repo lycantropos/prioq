a3d623cbf555c5b29bea7dd0e4c5cfc460f9e7dc
Azat Ibrakov <azatibrakov@gmail.com>
2022-06-12 08:49:38 +0700

    Complete tests of 'PriorityQueue' class

--- tests/test_peek.py
+++ tests/test_peek.py
@@ -16,5 +16,5 @@ def test_step(priority_queue: PriorityQueue) -> None:
     result = priority_queue.peek()
 
     assert result in priority_queue.values()
-    assert all(not item < priority_queue._item_factory(result)
-               for item in priority_queue._items)
+    assert all(not key < priority_queue._sorting_key(result)
+               for key, _ in priority_queue._items)
